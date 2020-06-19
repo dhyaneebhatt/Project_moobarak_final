7ad27636cc92c28ca5ed1efacf2541f2cc671571
Alay Desai <alaydesai094@gmail.com>
2020-06-19 11:57:14 -0400

    Initial Commit

---
+++ Project_moobarak_final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_moobarak_final
+//
+//  Created by Alay Desai on 2020-06-19.
+//  Copyright © 2020 Alay Desai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
